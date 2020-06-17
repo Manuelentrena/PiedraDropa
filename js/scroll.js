de75aa5dd71f30b8728a44fdd493277ddeca4ee5
Manuel <manuel.entrena@gmail.com>
2020-06-17 18:44:23 +0200

    Prototipo final 1.0

---
+++ js/scroll.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  var scrollY = window.scrollY;
+  var element = document.getElementById("header-content");
+  console.log(scrollY);
+  if (scrollY >= 150) {
+    element.classList.add("header-content-scroll");
+  } else {
+    element.classList.remove("header-content-scroll");
+  }
+};
+
+
+
+
